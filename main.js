e1a28a5ceec86afdabfa8f9a1aca297d04728d73
Rupesh Thapa Magar <117821106+rupeshmagar1@users.noreply.github.com>
2024-09-15 16:24:54 +0545

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var input = document.getElementById('email');
-            input.oninvalid = function(event) {
-    event.target.setCustomValidity('Please Enter your email id');
-}
-var input = document.getElementById('message');
-            input.oninvalid = function(event) {
-    event.target.setCustomValidity('Please text here');
-}
